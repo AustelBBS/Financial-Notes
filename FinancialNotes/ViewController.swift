c5b31ec793f9212007a3ca5b01bf7909d732c8db
Orlando Vergara <overgarah@orlandos-mac-mini.local>
2019-08-09 13:53:34 -0500

    Initial Commit

---
+++ FinancialNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinancialNotes
+//
+//  Created by Orlando Vergara on 8/9/19.
+//  Copyright © 2019 BSS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
